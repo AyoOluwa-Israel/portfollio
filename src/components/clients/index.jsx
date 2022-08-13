631539bd6bd125ed67b7d38d4a95c4cba7e86044
Israel AyoOluwa Adeleke <izzyfresher@gmail.com>
2022-08-13 02:45:23 +0100

    Improved spacing in hero section

--- src/components/clients/index.jsx
+++ src/components/clients/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Client = () => {
   return (
-    <div className="w-[85%] mx-auto ">
+    <div className="w-[85%] mx-auto mt-[2em]">
       <p className="text-yellow text-subheading ">Clients.</p>
       <p className="text-white lg:text-heading text-[24px] sm:w-[45%] w-[100%] font-secondary font-medium">
         Working with various brands provides me with invaluable experiences.
